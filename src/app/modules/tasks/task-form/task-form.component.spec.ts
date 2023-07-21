c6b53c2428f190c3c37b3b4197442f524fc6773b
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-21 10:48:33 +0300

    test(tasks): add unit tests for task-form component

--- src/app/modules/tasks/task-form/task-form.component.spec.ts
+++ src/app/modules/tasks/task-form/task-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskFormComponent } from './task-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
@@ -8,7 +8,8 @@ describe('TaskFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskFormComponent ]
+      declarations: [ TaskFormComponent ],
+      imports: [FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -19,5 +20,48 @@ describe('TaskFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Test for the form
+  it('should have a form with 1 control', () => {
+    expect(component.taskForm.contains('title')).toBeTruthy();
+  });
+
+  // Test title required
+  it('should make the title control required', () => {
+    const control = component.taskForm.get('title');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+  // Test tite maxlength
+  it('should make the title control max length 100', () => {
+    const control = component.taskForm.get('title');
+    control?.setValue(''.padEnd(101, 'a'));
+    expect(control?.valid).toBeFalsy();
+  });
+
+  // Test form submit
+  it('should submit the form', () => {
+    const control = component.taskForm.get('title');
+    control?.setValue('test');
+    expect(component.taskForm.valid).toBeTruthy();
+  });
+
+  // Test form reset
+  it('should reset the form', () => {
+    const control = component.taskForm.get('title');
+    control?.setValue('test');
+    component.onSubmit();
+    expect(control?.value).toBeNull();
+  });
+
+  // Test taskCreated event
+  it('should emit the taskCreated event', () => {
+    spyOn(component.taskCreated, 'emit');
+    const control = component.taskForm.get('title');
+    control?.setValue('test');
+    component.onSubmit();
+    expect(component.taskCreated.emit).toHaveBeenCalledWith('test');
   });
 });
