9e0e88611bf1d656e72af2a8606c73f1c31f8b26
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-23 14:32:40 +0300

    test: update e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,19 @@
 describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('type').click()
-    cy.url().should('include', '/commands/actions')
+  it('Visits the App', () => {
+    cy.visit('http://localhost:4200/');
+    // Test task being created
+    cy.get('[data-cy=task-input]').type('Test Task');
+    cy.get('[data-cy=task-input]').type('{enter}');
+    cy.get('[data-cy=task-list] [data-cy=task]').should('have.length', 1);
+    cy.get('[data-cy=task-list] [data-cy=task]').should('have.value', 'Test Task');
+    // Test task being marked as completed
+    cy.get('[data-cy=checkbox]').click();
+    cy.get('[data-cy=task-list] [data-cy=task]').should('have.class', 'completed');
+    // Test task being marked as uncompleted
+    cy.get('[data-cy=checkbox]').click();
+    cy.get('[data-cy=task-list] [data-cy=task]').should('not.have.class', 'completed');
+    // Test task being deleted
+    cy.get('[data-cy=delete]').click();
+    cy.get('[data-cy=task-list] [data-cy=task]').should('not.exist');    
   })
 })
