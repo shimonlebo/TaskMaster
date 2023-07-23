dd931b9cd3316bf364dd43bd4a5660d95410394c
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-23 15:06:21 +0300

    test: update e2e test & add data-cy attr to task-form and task-list

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,19 +1,17 @@
-describe('My First Test', () => {
-  it('Visits the App', () => {
+describe('Main Test', () => {
+  it('Creates, checks and deletes a task', () => {
     cy.visit('http://localhost:4200/');
-    // Test task being created
-    cy.get('[data-cy=task-input]').type('Test Task');
-    cy.get('[data-cy=task-input]').type('{enter}');
-    cy.get('[data-cy=task-list] [data-cy=task]').should('have.length', 1);
-    cy.get('[data-cy=task-list] [data-cy=task]').should('have.value', 'Test Task');
-    // Test task being marked as completed
+    // Test the input field
+    cy.get('[data-cy=task-input]').type('Test Task').type('{enter}');
+    cy.get('[data-cy=task-field]').should('have.length', 1);
+    cy.get('[data-cy=task-value]').should('have.value', 'Test Task');
+    // Test the checkbox
     cy.get('[data-cy=checkbox]').click();
-    cy.get('[data-cy=task-list] [data-cy=task]').should('have.class', 'completed');
-    // Test task being marked as uncompleted
+    cy.get('[data-cy=task-value]').should('have.class', 'completed');
     cy.get('[data-cy=checkbox]').click();
-    cy.get('[data-cy=task-list] [data-cy=task]').should('not.have.class', 'completed');
-    // Test task being deleted
+    cy.get('[data-cy=task-value]').should('not.have.class', 'completed');
+    // Test the delete button
     cy.get('[data-cy=delete]').click();
-    cy.get('[data-cy=task-list] [data-cy=task]').should('not.exist');    
-  })
-})
+    cy.get('[data-cy=task-field]').should('not.exist');
+  });
+});
