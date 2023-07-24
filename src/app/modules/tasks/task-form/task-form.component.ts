c87c1d3f900619a71ddfdea55815dbf7f60bbd42
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-24 13:13:54 +0300

    refactor: update task-form & task-list to use ngxs

--- src/app/modules/tasks/task-form/task-form.component.ts
+++ src/app/modules/tasks/task-form/task-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Store } from '@ngxs/store';
+import { AddTask } from '../task.state';
 
 @Component({
   selector: 'app-task-form',
@@ -7,29 +9,29 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./task-form.component.scss']
 })
 export class TaskFormComponent implements OnInit {
-  @Output() taskCreated = new EventEmitter<string>();
   taskForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
-  initializeForm() {
-    this.taskForm = this.fb.group({
-      title: ['', [Validators.required, Validators.maxLength(100)]]
-    });
-  }
-
   onSubmit() {
     if (this.taskForm.valid) {
-      console.log('Task Submitted');
-      this.taskCreated.emit(this.taskForm.value.title);
+      const id = this.store.selectSnapshot(state => state.tasks.tasks.length) + 1; 
+      const title = this.taskForm.value.title;    
+      this.store.dispatch(new AddTask({ id, title, completed: false }));
       this.taskForm.reset();
     } else {
       console.log('Invalid Task');
     }
+  }
+  
+  private initializeForm() {
+    this.taskForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.maxLength(100)])
+    });
   }
 
 }
--- src/app/modules/tasks/task-list/task-list.component.ts
+++ src/app/modules/tasks/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoTask as Task } from '../task.model';
 import { Select, Store } from '@ngxs/store';
-import { AddTask, EditTask, RemoveTask, ToggleCompleted } from '../task.state';
+import { EditTask, RemoveTask, ToggleCompleted } from '../task.state';
 import { Observable, map } from 'rxjs';
 
 @Component({
@@ -10,7 +10,7 @@ import { Observable, map } from 'rxjs';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-  @Select((state: { tasks: { tasks: any[]; }; }) => state.tasks.tasks) 
+  @Select((state: { tasks: { tasks: Task[]; }; }) => state.tasks.tasks) 
   tasks$: Observable<Task[]>;
 
   completedTasks$: Observable<number>;
@@ -23,24 +23,12 @@ export class TaskListComponent implements OnInit {
     );
   }
 
-  addTask(title: string): void {   
-    const task: Task = {
-      id: this.store.selectSnapshot(state => state.tasks.tasks.length) + 1,
-      title: title,
-      completed: false
-    };    
-    this.store.dispatch(new AddTask(task));
-    console.log('task added', task);
-  }
-
-  editTask(id: number, title: string) {
+  editTask(id: number, title: string): void {
     this.store.dispatch(new EditTask({ id, title }));
-    console.log(`task edited. id: ${id}, title: ${title}`);
   }
 
   deleteTask(id: number): void {
     this.store.dispatch(new RemoveTask(id));
-    console.log('task deleted. id:', id);
   }
 
   toggleCompleted(id: number): void {
