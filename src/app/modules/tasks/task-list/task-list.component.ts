f3a842f46734f6e1799a9f401492449f29d29bb6
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-24 11:43:50 +0300

    feat!: update task-list component to use ngxs

--- src/app/modules/tasks/task-list/task-list.component.ts
+++ src/app/modules/tasks/task-list/task-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoTask as Task } from '../task.model';
+import { Select, Store } from '@ngxs/store';
+import { AddTask, EditTask, RemoveTask, ToggleCompleted } from '../task.state';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
@@ -7,52 +10,47 @@ import { TodoTask as Task } from '../task.model';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-  tasks: Task[] = [];
-  completedTasks: number = 0;
+  @Select((state: { tasks: { tasks: any[]; }; }) => state.tasks.tasks) 
+  tasks$: Observable<Task[]>;
 
-  constructor() { }
+  completedTasks$: Observable<number>;
+
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.completedTasks$ = this.tasks$.pipe(
+      map(tasks => tasks.filter(task => task.completed).length)
+    );
+    console.log(this.store.snapshot());
+
   }
 
-  // TrackBy function to improve performance
-  trackByFn(index: number, task: Task): number {
-    return task.id;
-  }
-
-  addTask(title: string): void {
+  addTask(title: string): void {    
     const task: Task = {
-      id: this.tasks.length + 1,
-      title,
+      id: this.store.selectSnapshot(state => state.tasks.tasks.length) + 1,
+      title: title,
       completed: false
     };
-    this.tasks.push(task);
+    this.store.dispatch(new AddTask(task));
     console.log('task added', task);
   }
 
-  editTask(index: number, title: string) {
-    this.tasks[index].title = title;
-    console.log('task edited', this.tasks[index]);
+  editTask(id: number, title: string) {
+    this.store.dispatch(new EditTask({ id, title }));
+    console.log(`task edited. id: ${id}, title: ${title}`);
   }
 
   deleteTask(id: number): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
-    console.log('task deleted', id);
+    this.store.dispatch(new RemoveTask(id));
+    console.log('task deleted. id:', id);
   }
 
   toggleCompleted(id: number): void {
-    this.tasks = this.tasks.map(task => {
-      if (task.id === id) {
-        task.completed = !task.completed;
-        if (task.completed === true) {
-          this.completedTasks += 1;
-          console.log('task completed', id);
-        } else {
-          this.completedTasks -= 1;
-          console.log('task uncompleted', id);
-        }
-      }
-      return task;
-    });    
+    this.store.dispatch(new ToggleCompleted(id));   
+  }
+  
+  // TrackBy function to improve performance
+  trackByFn(index: number, task: Task): number {
+    return task.id;
   }
 }
