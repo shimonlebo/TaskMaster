44bf69225d032407b15ed32d80dcade13480361f
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2023-07-26 18:06:08 +0300

    fix: update file path in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgxsModule } from '@ngxs/store';
 
-import { TasksModule } from './modules/tasks/tasks.module';
+import { TasksModule } from './components/tasks/tasks.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
-import { TaskState } from './modules/tasks/task.state';
+import { TaskState } from './components/tasks/task.state';
 
 
 @NgModule({
