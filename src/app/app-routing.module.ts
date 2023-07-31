fb4be9dd061b083da9fa58bb6ac5605c585e4dd5
Simon Lebovits <58909312+shimonlebo@users.noreply.github.com>
2023-07-31 04:47:49 +0000

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './components/todos/todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', redirectTo: '/', pathMatch: 'full', 
+    component: TodoListComponent
+  },
 ];
 
 @NgModule({
